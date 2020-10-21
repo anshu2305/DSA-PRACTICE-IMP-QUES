053051d46d2b988073f7629d72cbc5668e054928
Anshika Sariya <56234295+anshu2305@users.noreply.github.com>
2020-10-21 11:48:59 +0530

    Create sol3.cpp

---
+++ Reverse_The_string/sol3.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+string reverseString(string S){
+  int n;
+  char temp;
+  n=S.length();
+  // Your code goes here
+  for(int i=0;i<n/2;i++){
+      temp=S[i];
+      S[i]=S[n-i-1];
+      S[n-i-1]=temp;
+  }
+  return S;
+}
+
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    string s;
+    cin>>s;
+    cout<<reverseString(s)<<endl;
+  }
+}
