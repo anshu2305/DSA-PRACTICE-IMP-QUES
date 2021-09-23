29daeea16753cc039cd39a2b2bcc6ba6c479f7e4
Anshika Sariya <56234295+anshu2305@users.noreply.github.com>
2021-09-23 19:58:46 +0530

    Create BinarySearch_Solution.cpp

---
+++ Aggressive_Cows_SPOJ_Problem/BinarySearch_Solution.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, C;
+long long A[100000];
+
+// check if a distance of x is possible b/w two cows
+bool canBePlaced(int A[], int n, int x)
+{
+  // greedy approach, put each cow in the first place you can
+  int cows_placed = 1, last_pos = A[0];
+  for (int i = 1; i < N; i++)
+  {
+    if ((A[i] - last_pos) >= x)
+    {
+      cows_placed++;
+      last_pos = A[i];
+    }
+    if (cows_placed == C)
+        return true;
+  }
+  return false;
+}
+
+void minDist(int A[], int N, int C)
+{
+  
+  sort(A, A + N);
+
+  // binary search 
+  long long low = 1, high = A[n-1] - A[0], mid, pos = -1;
+  while (high >= low)
+  {
+    mid = (high + low) / 2;
+    if (canBePlaced(mid))
+    {
+      low = mid + 1;
+      pos = mid;
+    }
+    else
+    {
+      high = mid - 1;
+    }
+  }
+  cout << pos << endl;
+}
+
+int main()
+{
+  int T;
+  cin >> T;
+  while (T--){
+    cin >> N >> C;
+    for (int i = 0; i < N; i++)
+    cin >> A[i];
+    solve(A, N, C);
+  }
+  return 0;
+}
