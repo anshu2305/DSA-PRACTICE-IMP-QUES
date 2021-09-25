8791d416166009130540dfe59371fc4c4c33b863
Anshika Sariya <56234295+anshu2305@users.noreply.github.com>
2021-09-25 19:25:36 +0530

    Update Memoisation_solution.cpp

--- Climbing_Stairs_leetcode/Memoisation_solution.cpp
+++ Climbing_Stairs_leetcode/Memoisation_solution.cpp
@@ -1,14 +1,18 @@
 //TIME COMPLEXITY = O(N) , SPACE COMPLEXITY = O(N)
 
 int Stairs(int n, vector <int> &dp){
+    if(n<0)
+        return 0;
     if(n==1 || n==0) 
         return 1;
+
     if(dp[n]!= -1)
         return dp[n];
     else{
         int x=Stairs(n-1,dp);
         int y=Stairs(n-2,dp);
-        return dp[n]= 1 + max( x , y);
+        dp[n]= x+y;
+        return dp[n];
     }
 }
 
