2ff2ce370cb37aa0a9e9e18bea09de7be39b5e74
Anshika Sariya <56234295+anshu2305@users.noreply.github.com>
2021-09-26 21:28:21 +0530

    Update solution.cpp

--- Kadane's_Algo/solution.cpp
+++ Kadane's_Algo/solution.cpp
@@ -1,13 +1,14 @@
 //Time Complexity= O(N) , Space Complexity = O(1) linear
 
-
 int maxSubarraySum(int arr[], int n){
     int max_sum=INT_MIN;
     int sum=0;
     for(int i=0;i<n;i++){
         sum=sum+arr[i];
-        if(sum>max_sum) max_sum=sum;
-        if(sum<0) sum=0;
+        if(sum>max_sum) 
+            max_sum=sum;
+        if(sum<0) 
+            sum=0;
     }
     return max_sum;
-  }
+}
