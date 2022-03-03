1e92d7cd78bab7c0ef1d9291c3f13aad0afecdc0
Anshika Sariya <56234295+anshu2305@users.noreply.github.com>
2022-03-03 16:43:18 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (33.83%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -5,12 +5,17 @@ public:
         
         for(int i=0;i<numRows ; i++){
             res[i].resize(i+1);
+            // each row contains i+1 elements so resize each row with i+1 column
+            
             res[i][0] = res[i][i] = 1;
+            //frst and last element of each row is always 1
             
             for(int j=1;j<i ; j++){
                 res[i][j]  = res[i-1][j-1] + res[i-1][j];
+                //each index is a sum of the j and j-1 column of last row
             }
         }
+        
         return res;
     }
 };
