5118ee54c06caa4803121e986316ad9dc82a36d5
Anshika Sariya <56234295+anshu2305@users.noreply.github.com>
2022-03-05 18:44:33 +0530

    Time: 26 ms (50.26%), Space: 8.4 MB (46.83%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -2,19 +2,23 @@ class Solution {
 public:
     
     int lengthOfLongestSubstring(string s) {
+        
         int left=0,right=0, count=0, n=s.length(), maxcount=0;
         map <char, int> mp;
         
         for(right; right<n; right++){
+            //if the elem doesnt exist put the elem with its index in map
             if(mp.count(s[right]) == 0){
                 mp.insert({s[right] , right});
             }
             else{
+                //if the elem exists and if the elem lies in current range [left,right] 
+                //then jump the left var by (ind) places  
                 int ind = mp[s[right]];
                 
                 if(ind >= left && ind<=right)
                     left = ind+1;
-                
+                //in both the cases  change the active index of elem in map
                 mp[s[right]] = right;
             }
             count= right -left+1;
@@ -22,5 +26,6 @@ public:
         }
         
         return maxcount;
+        //final time complexity is o(n)
     }
 };
