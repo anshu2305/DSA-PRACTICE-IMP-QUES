2e888dc527b197e7414cb4eca798d70aa7dffb5f
Anshika Sariya <56234295+anshu2305@users.noreply.github.com>
2022-03-09 20:49:45 +0530

    Time: 11 ms (50.83%), Space: 13.8 MB (73.22%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        /*
+        //twice traversal because the traversal is interrupted when it finds -ve number
+        
         long long maxprod = INT_MIN, prod=1;
         for(auto x: nums){
             prod = prod* x;
@@ -16,6 +19,27 @@ public:
                 prod =1;
         }
         return maxprod;
+        */
+        
+        //Approach 2
+        //if it encounters the negative then we swap mini with maxi 
+        //because current maxi will become minimum after multiplying with maxi 
+        //and vice versa
+        int m = nums[0];
+        int minn = nums[0];
+        int maxx = nums[0];
+        
+        
+         for(int i=1; i<nums.size(); i++){
+            if(nums[i]<0){
+                swap(minn, maxx);
+            }
+            maxx = max(nums[i], maxx * nums[i]);
+            minn = min(nums[i], minn * nums[i]);
+            m = max(m, maxx);
+        }
+        return m;  
+        
     }
     
 };
