babfae55c17156bc60f9e0c03d8acdbb6d945b0d
Anshika Sariya <56234295+anshu2305@users.noreply.github.com>
2022-03-09 23:00:09 +0530

    Time: 135 ms (42.21%), Space: 14.3 MB (65.50%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,25 +1,69 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        /*int n=coins.size();
-        if(n==0)
-            return 0;
-        int dp[val+1];
-        memset(dp , INT_MAX, sizeof(dp));
-        dp[0] = 0;
-        for(int j=0;j<n;j++){
-            for(int i=1; i<=val; i++){
-                if(coins[j] <= i){
-                    int subres= dp[i-coins[j]];
-                    if(subres != INT_MAX){
-                        dp[i] = min(subres + 1, dp[i]);
-                    }
-                }
-            }
+    
+    /*
+    Recursive approach:
+    
+    int coinChange(vector<int>& coins, int val){
+    
+    //base case for bottom up approach
+     if(val == 0)
+        return 0;
+      int res= INT_MAX; // for current call imaginary number of call are requires thats why we took int max  
+      
+      for(int i=0; i<n;i++){
+        if(coins[i] < val)
+        {
+            int subres = coinChange(coins, n, val-coins[i]); //stores res for recursive calls;
+            //if subres is not INTMAX that means some solution has been found during this recursive call
+            if(subres != INT_MAX)
+                res= min(res, res+1);
         }
-        if(dp[val]==INT_MAX) return -1;
-        return dp[val];
-        */
+      }
+      return res;
+    }
+    */
+
+    /*
+        Memoisation soln
+        int memo[10000];
+        
+        int solve(){
+            //base case for bottom up approach
+            if(val == 0)
+              return memo[val] =0;
+            if(memo[val] != -1)
+                return memo[val];
+            
+            int res= INT_MAX; // for current call imaginary number of call are requires thats why we took int max  
+
+              for(int i=0; i<n;i++){
+                if(coins[i] < val)
+                {
+                    int subres = coinChange(coins, n, val-coins[i]); //stores res for recursive calls;
+                    //if subres is not INTMAX that means some solution has been found during this recursive call
+                    
+                    if(subres != INT_MAX){
+                        
+                        res= min(res, subres+1);
+                        memo[val-coins[i]] = subres;
+                    }
+                        
+                }
+              }
+              return memo[val]=res;    
+        }
+        
+        int coinChange(vector<int>& coins, int val){
+            memo[val];
+            memset(memo, -1, sizeof(dp));
+            return sovle(coins, val);
+        }
+    */
+    
+    
+    int coinChange(vector<int>& coins, int amount) {
+        
         vector<int>dp(amount+1,INT_MAX);
        
         dp[0]=0;
@@ -28,7 +72,7 @@ public:
             for(int amt=1; amt<=amount; amt++ ){
                if(coin <= amt){
                    if(dp[amt-coin]!= INT_MAX)
-                   dp[amt] =min(1+dp[amt-coin],dp[amt]);
+                    dp[amt] =min(1+dp[amt-coin],dp[amt]);
                }        
             }
         }
